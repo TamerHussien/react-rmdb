c8c0ad5734aa141c3558253afea1a6b04f8d8761
Tamer Hussein <tamer.hussein@thoughtdesign.com.au>
2021-10-15 20:21:32 +0200

    create and integrate inital home page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 // components
 import Header from './components/Header';
+import Home from './components/Home';
 
 // styles
 import { GlobalStyle } from './GlobalStyle';
@@ -11,7 +12,7 @@ function App() {
   return (
     <div className="App">
       <Header/>
-      Start here.
+      <Home/>
       <GlobalStyle/>
     </div>
   );
---
+++ src/components/Home.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+// config
+import { POSTER_SIZE, BACKDROP_SIZE, IMAGE_BASE_URL } from "../config";
+// components
+
+// Hook
+import { useHomeFetch } from "../hooks/useHomeFetch";
+// Image
+
+import NoImage from '../images/no_image.jpg'
+
+const Home = () => {
+    const {state, loading, error} = useHomeFetch();
+    console.log(state, error,loading)
+    return <div>Home Page</div>
+}
+
+export default Home;
