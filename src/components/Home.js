d1bc272d88d1c58450af7c1c7ba54180d201767d
Tamer Hussein <tamer.hussein@thoughtdesign.com.au>
2021-10-16 18:21:49 +0200

    create Hero Component

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,7 @@ import React from "react";
 // config
 import { POSTER_SIZE, BACKDROP_SIZE, IMAGE_BASE_URL } from "../config";
 // components
-
+import HeroImage from "./HeroImage";
 // Hook
 import { useHomeFetch } from "../hooks/useHomeFetch";
 // Image
@@ -12,8 +12,23 @@ import NoImage from '../images/no_image.jpg'
 
 const Home = () => {
     const {state, loading, error} = useHomeFetch();
-    console.log(state, error,loading)
-    return <div>Home Page</div>
+    const hero = state?.results[0];
+    console.log(hero)
+
+    return (
+        <>
+
+        {hero ?
+            <HeroImage 
+                image={`${IMAGE_BASE_URL}${BACKDROP_SIZE}${hero.backdrop_path}`}
+                title={hero.original_title}
+                text={hero.overview}
+            /> 
+            : null
+        }
+
+        </>
+    )
 }
 
 export default Home;
