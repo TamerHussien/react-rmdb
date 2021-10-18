9106de624cbd42b3ad32ed8d41aa527bccf36c27
Tamer Hussein <tamer.hussein@thoughtdesign.com.au>
2021-10-18 20:10:12 +0200

    implement Routing with React Router

--- src/components/Thumb/index.js
+++ src/components/Thumb/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from 'react-router-dom';
 
 import {Image} from './Thumb.styles'
 
@@ -6,7 +7,17 @@ import {Image} from './Thumb.styles'
 const Thumb = ({image, movieId, clickable}) => (
 
     <div>
-        <Image src={image} alt='movie-thumb'/>
+        {clickable ? (
+
+            <Link to={`/${movieId}`}>
+                <Image src={image} alt='movie-thumb'/>
+            </Link>
+        )
+        : (
+            <Image src={image} alt='movie-thumb'/>
+        )
+    }
+        
     </div>
 );
 
